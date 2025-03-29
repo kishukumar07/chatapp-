260a4690601a4451dc2c9c2add52f6c6f0793b79
Kishukumar07 <kishukumars082@gmail.com>
2025-03-29 18:22:42 +0530

    userand room recieved at backend

--- Server/index.js
+++ Server/index.js
@@ -8,16 +8,33 @@ const app=express();
 const httpServer =  createServer(app); 
 const io =  new Server(httpServer); 
 
+io.on("connection" ,(socket)=>{
+    console.log("a user connected"); 
+
+socket.on("join-room",(data)=>{
+    console.log(data, "from serverside")
+})
+
+
+
+
+
+})
+
+
+
+
+
+
 
 const PORT = process.env.PORT ||= 4500;
 
 httpServer.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); 
 
 
-
-
+ 
 
 
 
