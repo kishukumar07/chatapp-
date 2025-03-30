9e31eec9c56b9cc37dcf303d9b19054b6b46b217
Kishukumar07 <kishukumars082@gmail.com>
2025-03-30 14:25:46 +0530

    yeah first part done

--- Server/helper/util.js
+++ Server/helper/util.js
@@ -1,36 +1,29 @@
 //tracking user records with room 
 
+
 const users = []; //each user consists of  id{using socket id here } , username , room 
 
-const getUser = (id) => {
-    return users.find((user) => {
-        if (user.id == id) return true;
-    })
+const getAUser = (id) => {
+    return users.find((user) => user.id == id);
 };
 
 const allUser = (room) => {
-    return users.filter((user) => {
-        if (user.room == room) return true;
-    })
+    return users.filter((user) => user.room == room);
 };
-
 
 const deleteUser = (id) => {
-
-    const index = users.findIndex((user) => {
-        if(user.id == id) return true;
-    })
-
-   if(index!=-1){ //if user is present 
-    return users.splice(index ,1)[0] ; //remove the particular user at particular index and return the arr. 
-   }
+    const index = users.findIndex((user) => user.id == id);
+    if (index != -1) {  //if user is present 
+        return users.splice(index, 1)[0];//remove the particular user at particular index and return the arr. 
+    }
 };
 
-const joinUser = (id ,username , room) => { 
-    const user = {username ,id , room}; 
-    users.push(user); 
-}; 
+const joinUser = (id ,name,room) => {
+  const user =  {id,name,room} ; 
+    users.push(user);
+    return user;
+};
 
 
 
-export  {getUser ,allUser ,deleteUser , joinUser }; 
+export  {getAUser ,allUser ,deleteUser , joinUser }; 
--- Server/index.js
+++ Server/index.js
@@ -1,35 +1,38 @@
 import express from 'express';
 
-import {createServer} from 'http'; 
-import {Server} from 'socket.io'
-import {getUser ,allUser ,deleteUser , joinUser } from './helper/util.js';  
-import { get } from 'https';
+import { createServer } from 'http';
+import { Server } from 'socket.io'
+import { getAUser ,allUser ,deleteUser , joinUser } from './helper/util.js';
 
 
 
 
-const app=express(); 
-const httpServer =  createServer(app); 
-const io =  new Server(httpServer); 
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 
 //connection 
 
-io.on("connection" ,(socket)=>{
-    console.log("a user connected ",socket.id); 
+io.on("connection", (socket) => {
+    console.log("a user connected ", socket.id);
 
-socket.on("join-room",(data)=>{
-    console.log(data, "from serverside")
-//Join the room 
-    socket.join(data.room); 
-//Emit a event to tell others that new user joined. 
-socket.broadcast.to(data.room).emit("new-user-joined" , data.name); 
-})
+    socket.on("join-room", (data) => {
+        // console.log(data, "from serverside"); 
 
+     
+        
+        //Join the room 
+        socket.join(data.room );
+        //mentioning track who joined in which room . 
+        joinUser(socket.id ,data.name,data.room);
 
+        //Emit a event to tell others that new user joined. 
+        socket.broadcast.to(data.room).emit("new-user-joined", data.name);
+        //also emitting the all users 
+        socket.broadcast.to(data.room).emit("room-users",allUser(data.room)); 
 
-
-
+    })
 });
 
 
@@ -40,12 +43,12 @@ socket.broadcast.to(data.room).emit("new-user-joined" , data.name);
 
 const PORT = process.env.PORT ||= 4500;
 
-httpServer.listen(PORT , () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-}); 
+});
 
 
- 
+
 
 
 
